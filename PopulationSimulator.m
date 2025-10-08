6c66fefdfea074a31ac0faa81f0895b0a0394d50
ArnavPrabhudesai <aoprabhudesai@wpi.edu>
2025-10-08 18:25:36 -0400

    0.1.1 Added Healing Mechanic (SIR Model)

--- PopulationSimulator.m
+++ PopulationSimulator.m
@@ -1,19 +1,20 @@
 % Grid size
-rows = 50;
-cols = 50;
+ROWS = 50;
+COLS = 50;
+MU = 5;       % average log scale
+SIGMA = 1;  % spread (higher = more extremes)
+SPREAD_RATE = 0.2;
+HEAL_RATE = 10;
 
-% Create random population density
-mu = 5;       % average log scale
-sigma = 1;  % spread (higher = more extremes)
-population = round(lognrnd(mu, sigma, rows, cols));
+population = round(lognrnd(MU, SIGMA, ROWS, COLS));
 
 % Display population as heatmap
-[x, y] = meshgrid(1:cols, 1:rows);  % grid coordinates
+[x, y] = meshgrid(1:COLS, 1:ROWS);  % grid coordinates
 x = x(:);  % flatten to vectors
 y = y(:);
 pop = population(:);  % flatten population matrix
 
-sizeGrid = reshape(dotSizes, rows, cols) / 100; % scale down
+sizeGrid = reshape(dotSizes, ROWS, COLS) / 100; % scale down
 
 % Scale dot sizes
 dotSizes = pop;           % size proportional to population
@@ -24,17 +25,25 @@ figure;
 h = scatter(x, y, dotSizes, ones(length(x),3), 'filled'); % start white
 axis off;
 
-r = zeros(rows, cols);
+r = zeros(ROWS, COLS);
+r_history = zeros(ROWS, COLS, HEAL_RATE);
 
 % Pick one random cell
-randRow = randi(rows)
-randCol = randi(cols)
+randRow = randi(ROWS)
+randCol = randi(COLS)
 
 % Give it a small red value
-r(randRow-1:randRow+1, randCol-1:randCol+1) = 0.2;   % Starting Intensity
+r(randRow-1:randRow+1, randCol-1:randCol+1) = 0.01;   % Starting Intensity
+
+b = zeros(ROWS, COLS);
+
+iter = 0;
 
 % Animation loop
 while true
+    r_history = cat(3, r, r_history(:,:,1:HEAL_RATE - 1));
+    iter = iter + 1;  
+
     % Pad both red and size matrices for border handling
     r_p = padarray(r, [1, 1], 'replicate');
     s_p = padarray(sizeGrid, [1, 1], 'replicate');
@@ -51,21 +60,20 @@ while true
         r_p(3:end, 2:end-1).*s_p(3:end, 2:end-1) + ...
         0.5 * r_p(3:end, 3:end).*s_p(3:end, 3:end);
 
-    % Compute recovery
-    neighborAntiSum = r_p(2:end-1, 2:end-1).*s_p(2:end-1, 2:end-1);
-    
-    spreadRate = 0.2;
-    healRate = 0.5;
-
-    r = r + spreadRate * neighborSum .* (1 - r) - healRate .* neighborAntiSum;
+    r = r + spreadRate * neighborSum .* (1 - r) - 0.8 .* (r_history(:,:,HEAL_RATE).^2);
     
     % Clamp to [0,1]
     r = max(0, min(r, 1));
-    g = 1 - r;
-    b = r*0.0001;
+    b = min(0.8 .* r_history(:,:,HEAL_RATE).^2 + b, 1);
+
+    g = 1 - r - b;
+
+    r(randRow, randCol)
+
+    r_history(randRow, randCol ,4)
     
     % Update scatter colors
     h.CData = [r(:), g(:), b(:)];
     drawnow;
-    pause(0.2);
+    pause(1);
 end
