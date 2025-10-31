06c0a1a24307921f90c1f19c2d9635fe00cc2c55
LordVanra <arnav.omkar@gmail.com>
2025-10-31 14:26:50 -0400

    0.2.8 Backend to Frontend connection completed (#16)
    Co-authored-by: Atharv Joshi <apjoshi@wpi.edu>

--- v-0.2-earthdata/backend/app.py
+++ v-0.2-earthdata/backend/app.py
@@ -1,19 +1,24 @@
 from flask import Flask, send_file, Response
+from flask_cors import CORS
 import time, os
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/live_frame')
 def live_frame():
     def generate():
         while True:
-            if os.path.exists("frame.jpg"):
-                with open("frame.jpg", "rb") as f:
+            if os.path.exists('frame.jpg'):
+                with open('frame.jpg', 'rb') as f:
                     frame = f.read()
                 yield (b"--frame\r\n"
-                       b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
-            time.sleep(0.1)
+                       b"Content-Type: image/jpeg\r\n\r\n"+ frame + b"\r\n")
+                time.sleep(0.5) #adjust as needed
     return Response(generate(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=True)
+    
---
+++ v-0.2-earthdata/my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import LiveSim from './LiveSim';
+import './CSS/index.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Epidemic Sandbox Simulator</h1>
+    <div>
+      <LiveSim />
+    </div>
+    </div>
+  );
+}
+
+export default App;
