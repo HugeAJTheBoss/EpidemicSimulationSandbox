9b4c4031618382b4feaa3691ce07171cb325b3c0
Atharv Joshi <apjoshi@wpi.edu>
2025-12-01 11:27:47 -0500

    0.2.2 Getting up to date

--- v-0.2-earthdata/my-react-app/src/components/SimulationContainer.jsx
+++ v-0.2-earthdata/my-react-app/src/components/SimulationContainer.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState } from "react";
 import PopulationSimButton from "./PopulationSimButton";
 import LiveSim from "./LiveSim";
@@ -5,17 +6,21 @@ import LiveSim from "./LiveSim";
 export default function SimulationContainer() {
   const [showSim, setShowSim] = useState(false);
 
-  const handleClick = () => {
-    setShowSim((prev) => !prev);
+  const handleButtonClick = () => {
+    setShowSim(prev => !prev);
   };
 
   return (
     <div className="centered">
-      <PopulationSimButton
-        label={showSim ? "Hide Simulation" : "Run Population Sim"}
-        onClick={handleClick}
-      />
-      {showSim && <LiveSim />}
+      <div onClick={handleButtonClick}>
+        <PopulationSimButton label={showSim ? "Hide Simulation" : "Population Sim"} />
+      </div>
+
+      {showSim && (
+        <div className="centered fade-in">
+          <LiveSim />
+        </div>
+      )}
     </div>
   );
 }
